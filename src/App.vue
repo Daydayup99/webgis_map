2cd6e1bdaf936df5ebd4dd6700f242b88c92ae0e
Daydayup99 <377578919@qq.com>
2022-08-07 02:15:55 +0800

    desription

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
-  <span>app</span>
-    </div>
+  <div>
+    <span>app</span>
+  </div>
 </template>
 
 <script>
-export default{
-  name:"app",
-  components:{}
-}
+export default {
+    name: 'app',
+    components: {},
+};
 </script>
 
 <style>
-html,body,#app {
- position: relative;
- width: 100%;
- height: 100%;
- margin: 0;
+html,
+body,
+#app {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    margin: 0;
 }
 </style>
