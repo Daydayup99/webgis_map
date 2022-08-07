63131db4b8f8e70935e181044f2d5ba38943cdbe
Daydayup99 <377578919@qq.com>
2022-08-07 16:09:42 +0800

    增加element

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-<<<<<<< HEAD
-  <div>
-    <span>app</span>
+  <div id="app">
+    <el-container class="outpanel">
+      <el-header class="sys-header">一张图系统</el-header>
+      <el-container class="contpanel">
+        <el-aside width="200px" class="sys-menu">左侧菜单</el-aside>
+        <el-main>地图区域</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
-<<<<<<< HEAD
-    name: 'app',
-=======
     name: 'App',
->>>>>>> 64ec1852fe669f67b47fe41b5b01a63b89fa33f2
     components: {},
 };
 </script>
@@ -24,8 +25,6 @@ body,
     width: 100%;
     height: 100%;
     margin: 0;
-<<<<<<< HEAD
-=======
 }
 .outpanel,
 .contpanel {
@@ -39,6 +38,5 @@ body,
 }
 .sys-menu {
     background-color: #c0c4cc;
->>>>>>> 64ec1852fe669f67b47fe41b5b01a63b89fa33f2
 }
 </style>
